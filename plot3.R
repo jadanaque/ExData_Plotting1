291611467ea2732c74d569e5e4a4744d4e0db264
jadanaque <javieradanaque@gmail.com>
2014-06-09 01:11:06 -0500

    creates plot3.png

---
+++ plot3.R
@@ -0,0 +1,21 @@
+epconsumption <- read.table("household_power_consumption.txt", header = T, sep = ";",
+                            nrows = 69518, na.strings = "?")
+
+# Subsetting just the rows corresponding to 1/2/2007 and 2/2/2007
+epconsumption <- epconsumption[66637:69516, ]
+
+# Converting Date and Time variables to their respective formats
+epconsumption$Time <- strptime(paste(epconsumption$Date, epconsumption$Time, sep=" "),
+                               "%d/%m/%Y %H:%M:%S")
+epconsumption$Date <- as.Date(epconsumption$Date, format = "%d/%m/%Y")
+
+# Plotting
+png("plot3.png")
+
+plot(epconsumption$Time, epconsumption$Sub_metering_1, type = "l",
+     ylab = "Energy sub metering", xlab = "")
+lines(epconsumption$Time, epconsumption$Sub_metering_2, col = 2)
+lines(epconsumption$Time, epconsumption$Sub_metering_3, col = 4)
+legend("topright", lty = 1, col = c(1, 2, 4), legend = tail(names(epconsumption), 3))
+
+dev.off()
